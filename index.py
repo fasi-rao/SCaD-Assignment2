786f346b50666ee789a99b5c3f3f4d83364d341c
awais <awaisrabbani711@gmail.com>
2023-06-11 21:54:21 -0700

    solve

---
+++ index.py
@@ -0,0 +1,15 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+  #include <stdio.h>
+ 
+int main(void)
+ {
+   int i = 10;
+   do {
+      printf("the value of i: %i\n", i);
+      i++;
+      }
+  while( i < 20 );
+ }
